e628a8db9768e20319116c26ca976e2deb94dc17
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-12 15:49:33 +0530

    user

---
+++ Stringdatatype/Demo.java
@@ -0,0 +1,12 @@
+public class Demo{
+    /*
+    1) demo file
+    2) using variables
+    */
+   public static void main(String args[]){
+    int num = 10; //this is int value
+    String name = "Krishna";
+    System.out.println(name);
+   }
+    
+}
