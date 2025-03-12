fb68a0fb05495fce6de984ec19f6f709a7045f6f
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-12 15:07:49 +0530

    :hi

--- command/Number.java
+++ command/Number.java
@@ -1,1 +1,10 @@
-hello
+public class Number{
+    /*
+    1)Number file
+    2)using variables
+*/
+public static void main(String args[]){
+    int num = 10; //this is int value
+    System.out.println(num);
+}
+}
