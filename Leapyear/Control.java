8643d7aa16ddad8ea44beb61358b7b42d3e5db8a
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-13 14:47:18 +0530

    realworld oject

---
+++ Leapyear/Control.java
@@ -0,0 +1,11 @@
+public class Control{
+    public static void main(String args[]){
+        int i=2020;
+        if(i%4==0){
+         System.out.println("leap year");
+         }
+         else{
+        System.out.println("Notleap Year");  
+        }
+    }
+}
