52b341f5378073d155c35dc3577d0b3bb5207a42
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-13 12:23:15 +0530

    good afternoon

---
+++ Ifelsestatements/Neutral.java
@@ -0,0 +1,15 @@
+public class Neutral{
+    public static void main(String args[]){
+    int num = 0;
+    if(num>=-1){
+        System.out.println("Positive");
+    }
+        else if(num==0){
+            System.out.println("Neutral");
+        }
+        else{
+            System.out.println("Negative");
+        }
+    }
+}
+    
