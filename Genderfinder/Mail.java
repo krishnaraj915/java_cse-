6fa81e53f326e37f9a3fbd85a2d6471a81c890e6
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-13 12:03:29 +0530

    rose flower

---
+++ Genderfinder/Mail.java
@@ -0,0 +1,11 @@
+public class Mail{
+    public static void main(String args[]){
+    String Gender="m";
+    if(Gender=="m"){
+    System.out.println("Female");
+     }
+     else{
+    System.out.println("Male");
+    }
+}
+}
