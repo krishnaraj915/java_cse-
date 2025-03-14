ee0a596eba76b649438dc0a36319a9d5193abb71
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-14 12:10:41 +0530

    hlooo programmer

---
+++ Indexof/Indexes.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Indexes{
+    public static void main(String args[]){
+        String name = "hello,world";
+        System.out.println(name.indexOf("w"));
+    }
+}
