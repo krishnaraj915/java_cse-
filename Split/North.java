b9a38252a5aa3fc589dc5417772bd3d92a2fc132
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-14 11:50:18 +0530

    java programmer

---
+++ Split/North.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class North{
+    public static void main(String args[]){
+        String name = "hello,world";
+        String names[] = name.split(",");
+        System.out.println(names[0]);
+
+    }
+}
