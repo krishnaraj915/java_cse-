6be3d997f5a62257acc3958c8044d86b0f840b74
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-14 15:00:09 +0530

    :this is input

--- Multipleinputs/Input.java
+++ Multipleinputs/Input.java
@@ -6,14 +6,15 @@ public class Input{
         int n = input.nextInt();
         int a[] = new int[n];
         int i =0;
+        int j =0;
         while(i<n){
             System.out.println("Enter the value:");
             a[i] = input.nextInt();
             i = i+1;
         }
-        while(i<n){
-            System.out.println("Entered the value:"+a[i]);
-            i = i+1;
+        while(j<n){
+            System.out.println("Entered the value:"+a[j]);
+            j = j+1;
         }
         }
 }
