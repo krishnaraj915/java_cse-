67c447fd084bc9a59f5c5cdb59e60917229372dc
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-14 13:11:52 +0530

    datastructure

---
+++ Casess/casesss.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Casesss{
+    public static void main(String args[]){
+    String name = "hii,world";
+    System.out.println(name.toUpperCase());
+    System.out.println(name.toLowerCase());
+
+}
+}
