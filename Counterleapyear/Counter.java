e0fa8ffc1e934c93891a4a3d1db2efe1edf28053
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-14 10:56:52 +0530

    new day

---
+++ Counterleapyear/Counter.java
@@ -0,0 +1,11 @@
+public class Counter{
+    public static void main(String args[]){
+        int count=0;
+        for(int i=2020;i<2030;i++){
+            if(i%4==0){
+                count=count+1;
+            }
+        }
+        System.out.println(count);
+    }
+}
