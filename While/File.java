375b556153a3c39567deaafb323aad1c68e64ccd
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-14 10:03:07 +0530

    programming

---
+++ While/File.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class File{
+    public static void main(String args[]){
+        int i = 92;
+         while(i<98){
+            System.out.println(i);
+            i=i+1;
+         }
+    }
+}
