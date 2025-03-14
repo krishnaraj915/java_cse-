47c99e2241ad7d445df3ae0b86f291d803f32f94
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-14 10:31:40 +0530

    git hub

---
+++ Seventhtable/Oops.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Oops{
+    public static void main(String args[]){
+    int i = 0;
+    int x = 0;
+    while(i<70){
+        i=i+1;
+        x=x+1;
+        System.out.println(x+"x7"+1);
+    }
+    }
+}
