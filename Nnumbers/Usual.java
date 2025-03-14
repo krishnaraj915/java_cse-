56b5fc92e260998e771195540cc4ad5f9c972360
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-14 11:12:20 +0530

    usual programming

---
+++ Nnumbers/Usual.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Usual{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=input.nextInt();
+        int i=1;
+        while(i<=num)
+        {
+            System.out.println(i);
+            i=i+1;
+        }
+    }
+    }
