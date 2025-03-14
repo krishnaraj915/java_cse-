a96232db855a12f2c4bb37e2163de977f1b2fe89
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-14 09:38:17 +0530

    im a developer

---
+++ Majorminor/Minor.java
@@ -0,0 +1,12 @@
+public class Minor{
+    public static void main(String args[]){
+        int age=19;
+        if(age>=18){
+        System.out.println("The person is Major");
+    }
+    else
+    {
+        System.out.println("The person is Minor");
+    }
+}
+}
