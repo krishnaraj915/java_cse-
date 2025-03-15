06215b822a6b0da70e87c309ba8a5a61e60eefd6
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-15 10:49:05 +0530

    constructor in java

---
+++ Constructor/Cons.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Cons{
+    Cons(){
+        System.out.println("krishna");
+    }
+    public static void main(String args[]){
+        Cons obj = new Cons();
+    }
+}
