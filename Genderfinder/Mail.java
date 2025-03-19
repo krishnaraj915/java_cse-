7fb47fa0904799b0047176ddc48200a641660b26
krishnaraj915 <arkrishna20057@gmail.com>
2025-03-19 12:52:42 +0530

    Update Mail.java

--- Genderfinder/Mail.java
+++ Genderfinder/Mail.java
@@ -1,7 +1,7 @@
 public class Mail{
     public static void main(String args[]){
-    String Gender="m";
-    if(Gender=="m"){
+    String Gender="f";
+    if(Gender=="f"){
     System.out.println("Female");
      }
      else{
